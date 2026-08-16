d739ab5f2ddb232b8f1bd39d9ed3fcbb324187d0
Mike20260810 <hjlee2020@gmail.com>
2026-08-16 20:18:20 +0900

    Update headerNavLinks.ts

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,8 +1,7 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
+  { href: '/blog', title: 'Articles' },
+  { href: '/tags', title: 'Topics' },
   { href: '/about', title: 'About' },
 ]
 
