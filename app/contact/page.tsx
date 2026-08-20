ee64c7423951477723fdaa1163491d0cfe800be3
Mike20260810 <hjlee2020@gmail.com>
2026-08-20 21:44:12 +0900

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,17 +7,22 @@ export default function ContactPage() {
   return (
     <div className="divide-y divide-gray-200 dark:divide-gray-700">
       <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+        <h1 className="leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
           Contact
         </h1>
       </div>
-      <div className="prose max-w-none pt-8 pb-8 dark:prose-invert">
+      <div className="dark:prose-invert max-w-none pt-8 pb-8">
         <p>
-          If you have any questions, feedback, or business inquiries, feel free to reach out to us!
+          If you have any questions, feedback, or business inquiries, feel free
+          to reach out to us!
         </p>
         <ul>
-          <li><strong>Email:</strong> (hjlee2020@gmail.com)</li>
-          <li><strong>Website:</strong> https://www.hobiknows.com/</li>
+          <li>
+            <strong>Email:</strong> (hjlee2020@gmail.com)
+          </li>
+          <li>
+            <strong>Website:</strong> https://www.hobiknows.com/
+          </li>
         </ul>
       </div>
     </div>
